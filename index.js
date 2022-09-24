28bee3646eca3d0a3a1e5368c975b66cd8bda199
ManhNhhhhh <manhnhgch18499@fpt.edu.vn>
2022-09-24 16:19:29 +0700

    up

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-const http = require("htpp");
-http.createserver((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type" , "text/html");
-    res.end("<h1>Heroku deployment tutorial<h1>");
-})
-.listen()
+const { fstat } = require('fs');
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  if (req.url === "/") {
+    fs.readFile("./index.html", (err, data) =>{
+        if (err) {
+            console.error(err);
+        } else {
+            res.write(data);
+            res.end;
+        }
+    });
+  } else if (req.url === "/greenwich"){
+    fs.readFile("./greenwich.html", (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.write(data);
+            res.end;
+        }
+    });
+  } else if (req.url === "/cloud"){
+    fs.readFile("./cloud.html", (err, data) => {
+        res.write(data);
+        req.end;
+    });
+    
+  } else {
+    res.end("<h1>404 : Page not found</h1<");
+  }
+
+  })
+
+.listen(process.env.PORT || 3000) ;
